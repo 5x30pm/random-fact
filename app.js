b8cad5d305bedca07aef7b3c9da18ac2f3bdd8cc
Shiv Ranjan <5x30pm@proton.me>
2026-05-31 11:30:32 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-let btn = document.querySelector("button");
-btn.addEventListener('click', async () =>{
+let btn = document.querySelector("#get-fact");
+let url = 'https://catfact.ninja/fact'
+
+btn.addEventListener('click', async () => {
     let fact = await getFact();
+
+    // select para and write fact 
     let para = document.querySelector("#result");
+    para.classList.add("result-box");
     para.innerText = fact;
+
+    //change btn to Get More Fact
+    btn.innerText = 'Get More Fact';
+
 });
 
-
-
-
-
-
-let url = 'https://catfact.ninja/fact'
+// function for getting fact from api
 async function getFact() {
     try {
         let res = await axios.get(url);
         // console.log(res.data.fact);
         return res.data.fact;
 
-    } catch (err){
+    } catch (err) {
         console.log("error - ", err);
         return 'No Fact Found!';
     }
